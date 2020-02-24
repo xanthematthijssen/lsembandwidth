60ec5fc5ba1cf8c6c6254c68bf5b0f0c76ba9673
Xanthe Matthijssen Laptop <xanthematthijssen@gmail.com>
2020-02-24 21:47:20 +0100

    create license

--- tools/main_drabbel.R
+++ tools/main_drabbel.R
@@ -1,11 +1,14 @@
-
-
 library(devtools)
 rm(list = ls())
+
 load_all()
 check()
-
+##### testing #####
 data <- cbind(rnorm(100), runif(100))
-# build ignore
-usethis::use_build_ignore(c("R_drabbels"))
 create_crossvaldata(data=data, K = 5)
+
+#####  build ignore#####
+usethis::use_build_ignore(c("R_drabbels"))
+
+#### MIT license ######
+use_mit_license("Xanthe Matthijssen")
