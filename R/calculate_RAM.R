1c111e97139f2725dd7d892bfcb66eb0c3e475cb
Xanthe Matthijssen werk <xanthematthijssen@gmail.com>
2020-04-06 17:34:49 +0200

    Update RAM_calculation of mean vector

--- R/calculate_RAM.R
+++ R/calculate_RAM.R
@@ -1,57 +1,81 @@
-
-#' @importFrom magrittr %>%
+#' Calculates RAM matrices from dataframe of fittted values
+#'
+#'
+#' @param df_pars_onemod df of estimated parameters of lsem model for one value of the moderator
+#' @return list:
+#' value_moderator = value of moderator for the matrices;
+#' vector_means = estimated means of multivariate normal;
+#' matrix_covariance = estimated covariance of multivariate normal;
+#' vector_variablenames = sequence variable names in which all matrices / vectors are ordered;
+#'
 #' @importFrom rlang .data
+#' @importFrom magrittr %>%
 
 
 calculate_RAM <- function(df_pars_onemod) {
-  variable_vector <- unique(c(df_pars_onemod$lhs, df_pars_onemod$rhs))
-  variable_vector <- variable_vector[variable_vector != ""]
+  vector_variablenames <- unique(c(df_pars_onemod$lhs, df_pars_onemod$rhs))
+  vector_variablenames <- vector_variablenames[vector_variablenames != ""]
 
-  n_vars <- length(variable_vector)
+  n_vars <- length(vector_variablenames)
 
+  #input dataframe is grouped, so ungroup it to avoid errors
   df_pars_onemod <- dplyr::ungroup(df_pars_onemod)
 
-  fitted_means <- df_pars_onemod %>% dplyr::filter(.data$op == "~1")
-  fitted_covs <- df_pars_onemod %>% dplyr::filter(.data$op == "~~")
+  # split up data frame in types
+  fitted_intercepts <- df_pars_onemod %>% dplyr::filter(.data$op == "~1")
+  fitted_covariances <- df_pars_onemod %>% dplyr::filter(.data$op == "~~")
   fitted_loadings <- df_pars_onemod %>% dplyr::filter(.data$op == "=~")
+  fitted_betas <- df_pars_onemod %>% dplyr::filter(.data$op == "~")
 
-  mod_value <- df_pars_onemod$sample_mods[1]
+  value_moderator <- df_pars_onemod$sample_mods[1]
 
-  # mean vector
-  mean_vector <- fitted_means$weighted_est
-  names(mean_vector) <- fitted_means$lhs
-  mean_vector <- mean_vector[variable_vector]
-  mean_vector[is.na(mean_vector)] <- 0
-  names(mean_vector) <- variable_vector
+  # make intercept vector (Theta)
+  vector_intercepts <- fitted_intercepts$weighted_est
+  names(vector_intercepts) <- fitted_intercepts$lhs
+  vector_intercepts <- vector_intercepts[vector_variablenames]
+  vector_intercepts[is.na(vector_intercepts)] <- 0
+  names(vector_intercepts) <- vector_variablenames
 
-  #covariances
-  fitted_covs <-
-    rbind(fitted_covs,
-          fitted_covs %>%
+  # make regression and loading matrix (Beta)
+  matrix_beta <-
+    make_full_matrix(rbind(fitted_loadings, fitted_betas),
+                     vector_variablenames = vector_variablenames)
+
+  # (co-)variance matrix (Eta)
+  fitted_covariances <-
+    rbind(fitted_covariances,
+          fitted_covariances %>%
             dplyr::mutate(hs = .data$lhs,
                           lhs = .data$rhs,
                           rhs = .data$hs) %>%
             dplyr::select(-.data$hs))
-  fitted_covs <- fitted_covs[!duplicated(fitted_covs),]
+  fitted_covariances <- fitted_covariances[!duplicated(fitted_covariances),]
 
-  cov_matrix <-
-    make_full_matrix(fitted_covs, variable_vector = variable_vector)
+  matrix_additional_covariance <-
+    make_full_matrix(fitted_covariances, vector_variablenames = vector_variablenames)
 
-  # loadings
-  load_matrix <-
-    make_full_matrix(fitted_loadings, variable_vector = variable_vector)
 
-  # combine
-  est_matrix <-
-    solve(diag(length(variable_vector)) - load_matrix) %*%
-    cov_matrix %*% t(solve(diag(length(variable_vector)) - load_matrix))
+
+  # combine into multivariate normal covariance matrix and expected value vector
+  # if (diag(n_vars) - matrix_beta) is invertable
+  if (det(diag(n_vars) - matrix_beta) > 0) {
+    matrix_covariance <-
+      solve(diag(n_vars) - matrix_beta) %*%
+      matrix_additional_covariance %*%
+      t(solve(diag(n_vars) - matrix_beta))
+
+    vector_means <-
+      solve(diag(n_vars) - matrix_beta) %*%
+      vector_intercepts
+  } else {
+    cat("diag minus beta matrix is not invertable")
+  }
+
 
   return(list(
-    mod_value = mod_value,
-    mean_vector = mean_vector,
-    variable_vector = variable_vector,
-    cov_matrix = cov_matrix,
-    est_matrix = est_matrix,
-    load_matrix = load_matrix
+    value_moderator = value_moderator,
+    vector_means = vector_means,
+    vector_variablenames = vector_variablenames,
+    matrix_covariance = matrix_covariance
   ))
 }
